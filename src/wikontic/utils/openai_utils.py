9bbeb0e4ac5fe42c1ecc027d6a66f9cc5bcd02b1
gilbert <mplgg@MSI>
2026-05-20 14:13:15 +0200

    add gpt-oss-20b for future proofing

--- src/wikontic/utils/openai_utils.py
+++ src/wikontic/utils/openai_utils.py
@@ -38,6 +38,7 @@ class LLMTripletExtractor:
         "Meta-llama/Llama-3.3-70B-Instruct": {"input": 0.04, "output": 0.12},
         "qwen/qwen3-32b": {"input": 0.05, "output": 0.2},
         "Qwen/Qwen3-32B": {"input": 0.05, "output": 0.2},
+        "openai/gpt-oss-20b": {"input": 0.05, "output": 0.2},
         "openai/gpt-oss-120b": {"input": 0.05, "output": 0.2},
         "Openai/Gpt-oss-20b": {"input": 0.05, "output": 0.2},
         "Openai/Gpt-oss-120b": {"input": 0.05, "output": 0.2},
